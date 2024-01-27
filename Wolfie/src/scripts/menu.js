c5f46a704ee150fec7a57a81d61ac0c5a721d45a
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-01-27 11:26:29 +0200

    Added responsive drawer menu for smaller screens

---
+++ Wolfie/src/scripts/menu.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger-icon').addEventListener('click', () => {
+  
+  document.getElementById('topnav').classList.toggle('responsive');
+  document.getElementById('hamburger').classList.toggle('open');
+});
