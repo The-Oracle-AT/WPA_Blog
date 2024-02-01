2713c80607f15015d800c7d50acf73bbee1fdb14
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-02-01 21:46:34 +0200

    Switched from md -> mdv posts. Added sorting to the newest articles so it displays the latest 4 by date

--- Wolfie/src/content/config.ts
+++ Wolfie/src/content/config.ts
@@ -6,11 +6,13 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 		tags: z.array(z.string()),
+		link: z.string(),
 
 	}),
 	
