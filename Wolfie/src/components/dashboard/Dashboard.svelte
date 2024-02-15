2a465ad8455681f8f79245b3ec21e95282bb4a27
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-02-15 17:01:23 +0200

    Added more pages and some UI to structure the website and visualize

---
+++ Wolfie/src/components/dashboard/Dashboard.svelte
@@ -0,0 +1,60 @@
+<script>
+    import Card from "../svelte/Card.svelte";
+
+</script>
+
+<div class="dashboard">
+    <div class="dashboard-content">
+        <div class="progress">
+            <Card title="Progress">
+                <p>Progress content goes here</p>
+            </Card>
+        </div>
+        <div class="achievements">
+            <Card title="Achievements">
+                <p>Goals content goes here</p>
+            </Card>
+        </div>
+        <div class="tasks">
+            <Card title="Tasks">
+                <p>Tasks content goes here</p>
+            </Card>
+        </div>
+        <div class="calendar">
+            <Card title="Calendar">
+                <p>Calendar content goes here</p>
+            </Card>
+        </div>
+    </div>
+
+</div>
+
+
+<style lang="scss">
+    .dashboard {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        :h1 {
+            align-self: flex-start;
+        }
+    }
+
+    @media (min-width: 768px) {
+        .dashboard {
+            padding: 2rem;
+        }
+        .dashboard-content {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        
+    }
+    }
+
+    
+
+
+
+</style>
---
+++ Wolfie/src/components/svelte/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let title = "Title";
+    export let content = "Content";
+    export let border = "1px solid #000";
+    export let backgroundColor = "#fff";
+    
+    $: console.log(title);
+    $: console.log(content);
+</script>
+
+<div class="card" style="--borderSettings: {border}; --backgroundSettings: {backgroundColor} ">
+    <h2>{title}</h2>
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        border: var(--borderSettings);
+        background: var(--backgroundSettings);
+        padding: 1rem;
+        margin: 1rem;
+        border-radius: 1rem;
+    }
+</style>
