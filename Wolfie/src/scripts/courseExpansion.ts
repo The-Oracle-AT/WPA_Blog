c75c29369dc1208c755c4883e752247a23ac4a37
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-03-21 20:19:34 +0200

    fix: Users can now click on icon to see all lessons under the category

--- Wolfie/src/scripts/courseExpansion.ts
+++ Wolfie/src/scripts/courseExpansion.ts
@@ -1,28 +1,13 @@
-document.addEventListener("astro-load", function() {
-    const sections = document.querySelectorAll("#courses section") as NodeListOf<HTMLElement>;
-    console.log(sections);
-    
-    sections.forEach(section => {
-      const title = section.querySelector("h2") as HTMLElement;
-      const list = section.querySelector("ul")  as HTMLElement;
-      const toggleIcon = title.querySelector(".toggle-icon") as HTMLElement;
-
-      title.addEventListener("click", function() {
-        const isVisible = list.classList.toggle("visible");
-        if (isVisible) {
-          sections.forEach(otherSection => {
-            if (otherSection !== section) {
-              const otherList = otherSection.querySelector("ul");
-              if (otherList) {
-                otherList.classList.remove("visible");
-              }
-            }
-          });
-          toggleIcon.textContent = "-";
-        } else {
-          toggleIcon.textContent = "+";
-        }
-      });
+const toggleIcons = document.querySelectorAll('.toggle-icon') as NodeListOf<HTMLElement>;
+const courseSections = document.querySelectorAll('.course-section') as NodeListOf<HTMLElement>;
+toggleIcons.forEach((icon:any) => {
+    icon.addEventListener('click', () => {
+        const courseSection = icon.parentElement?.parentElement?.parentElement as HTMLElement;
+        courseSection.classList.toggle('visible');
     });
-  });
-  
+
+
+
+});
+
+// 
