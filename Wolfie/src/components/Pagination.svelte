5cd8d8be08a8ba7b515206dbcd6211e3a6c13089
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-04-30 12:14:13 +0200

    Successfully added pagination and nested pagination.

---
+++ Wolfie/src/components/Pagination.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let prevousPageURL;
+    export let nextPageURL;
+
+    $:showPrevURL = prevousPageURL ? 'show' : 'hide'
+    $:showNextURL = nextPageURL ? 'show' : 'hide'
+    
+
+</script>
+
+<div class="pagination_wrapper">
+    
+    <a href={prevousPageURL} class={`prev ${showPrevURL}`} >Prevous</a>
+    <a href={nextPageURL} class={`next ${showNextURL}`}>Next</a>
+</div>
+
+<style lang="scss">
+    div {
+        position: relative;
+       
+        width: 100%;
+    }
+    .next{
+        position: absolute;
+        right: 1rem;
+       
+        &.show {
+            color: inherit;
+            display: block;
+        }
+        &.hide {
+            display: none;
+        }
+        
+    }
+    .prev {
+        position: absolute;
+        left:1rem;
+        
+        &.show {
+            color: inherit;
+            display: block;
+        }
+        &.hide {
+            display: none;
+        }
+    }
+    
+</style>
