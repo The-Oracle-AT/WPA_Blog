e425b8d1474b8d01220adcf49389461af2c6e0c1
Lapane <96382250+The-Oracle-AT@users.noreply.github.com>
2024-05-04 03:34:44 +0200

    Fixed in posts pagination and added some styling to the bottom nav links

--- Wolfie/src/components/Pagination.svelte
+++ Wolfie/src/components/Pagination.svelte
@@ -1,17 +1,40 @@
 <script>
+    import { onDestroy, onMount } from "svelte";
+    
     export let prevousPageURL;
     export let nextPageURL;
+    
 
     $:showPrevURL = prevousPageURL ? 'show' : 'hide'
     $:showNextURL = nextPageURL ? 'show' : 'hide'
+
+    let theme = 'light'
+    $: dark = theme
+    onMount(() => {
+        theme = window.localStorage.getItem('theme')
+        document.getElementById("themeToggle").addEventListener("click", handleToggleClick);
+    }) 
+    function handleToggleClick(e) {
+        if(theme === 'light') { 
+            theme = 'dark'
+            
+        } else { 
+            theme = 'light'
+        } 
+        console.log('clicked theme button')
+    }
+    
+    
+    onDestroy(() => {
+    })
     
 
 </script>
 
-<div class="pagination_wrapper">
+<div class={`pagination_wrapper ${dark}`}>
     
-    <a href={prevousPageURL} class={`prev ${showPrevURL}`} >Prevous</a>
-    <a href={nextPageURL} class={`next ${showNextURL}`}>Next</a>
+    <a href={prevousPageURL} class={`prev ${showPrevURL} `} ><slot name="prev_link_content"><span>Prevous</span></slot> </a>
+    <a href={nextPageURL} class={`next ${showNextURL} `}><slot name="next_link_content"><span>Next</span></slot> </a>
 </div>
 
 <style lang="scss">
@@ -19,6 +42,40 @@
         position: relative;
        
         width: 100%;
+        color: inherit;
+        
+        &.pagination_wrapper {
+            &.light {
+                color: black;
+            }
+            &.dark {
+                color: white;
+            }
+        }
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        width: 40%;
+        height: 4rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 3px 6px 3px rgba(0, 0, 0, 0.5);
+        padding-left: 1rem;
+        padding-right: 1rem;
+        background-color: var(--light-nav);
+        border: 0.5px var(--accent) solid;
+        &:first-child{
+            border-top-right-radius: .5rem;
+            border-bottom-right-radius: .5rem;
+
+        }
+        &:last-child{
+            border-top-left-radius: .5rem;
+            border-bottom-left-radius: .5rem;
+        }
+        
     }
     .next{
         position: absolute;
@@ -27,6 +84,7 @@
         &.show {
             color: inherit;
             display: block;
+           
         }
         &.hide {
             display: none;
@@ -38,8 +96,9 @@
         left:1rem;
         
         &.show {
-            color: inherit;
+           
             display: block;
+            
         }
         &.hide {
             display: none;
